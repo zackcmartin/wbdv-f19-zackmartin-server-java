290c1c915812d1ce12cf6372d4ba8b8bed8646d2
Zack Martin <zackmartin@Zacks-MacBook-Pro-2.local>
2019-11-21 17:15:21 -0500

    assignment 6 resubmission

--- src/main/java/com/example/wbdvf19zackmartinserverjava/controllers/ModuleController.java
+++ src/main/java/com/example/wbdvf19zackmartinserverjava/controllers/ModuleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ModuleController {
 
     @Autowired
--- src/main/java/com/example/wbdvf19zackmartinserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvf19zackmartinserverjava/controllers/WidgetController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class WidgetController {
 
     @Autowired
